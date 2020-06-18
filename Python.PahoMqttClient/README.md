d43678efeb0644999b334156b508ad3e5b8a2ef2
Adam Szymanski <adam.szymanski@pearson.com>
2020-06-18 22:22:39 +0200

    Send json data

--- Python.PahoMqttClient/README.md
+++ Python.PahoMqttClient/README.md
@@ -1,9 +1,52 @@
-# playground
+# Playground of sending MQTT messages from Python to Hassio via MQTT
 Python MQTT Client
 
-How to run?
+# How to run?
 ```
 python -m pip install --upgrade pip
-pip install paho-mqtt
+pip install -r requirements.txt
 python publish_mqtt.py
+```
+
+# Setup
+Create `secrets.py` file and fill data
+```
+broker="ip_address_of_your_mqtt_server"
+port=1883
+username="mqtt_username"
+password="mqtt_password"
+```
+
+# Handle data in Hassio
+Define custom sensor
+Add into `configuration.yaml` line:
+```
+sensor: !include customize_sensors.yaml
+```
+
+Define custom MQTT sensors in `customize_sensors.yaml`
+```
+- platform: mqtt
+  name: Current waterflow
+  icon: mdi:water
+  state_topic: "greenhouse/waterflow"
+  unit_of_measurement: 'l/min'
+  value_template: "{{ value_json.current }}"
+  
+- platform: mqtt
+  name: Today waterflow
+  icon: mdi:water
+  state_topic: "greenhouse/waterflow"
+  unit_of_measurement: 'l/min'
+  value_template: "{{ value_json.today }}"
+```
+
+Display sensor values in lovelace:
+```
+- type: entities
+  show_header_toggle: false
+  title: Water flow
+  entities:
+    - sensor.current_waterflow
+    - sensor.today_waterflow
 ```
--- Python.PahoMqttClient/publish_mqtt.py
+++ Python.PahoMqttClient/publish_mqtt.py
@@ -1,17 +1,36 @@
 #!/usr/bin/env python3
+
 import secrets
 import paho.mqtt.client as paho
+import json
 
-def on_publish(client,userdata,result):
+MQTT_TOPIC_WATERFLOW = "greenhouse/waterflow"
+MQTT_TOPIC_WATERTANK = "greenhouse/watertank"
+
+client = paho.Client()
+
+def on_publish(client, userdata, result):
     print("data published \n")
     pass
-client = paho.Client()
-client.on_publish = on_publish
-client.username_pw_set(secrets.username, secrets.password)
-client.connect(secrets.broker, secrets.port)
-ret = client.publish("greenhouse/waterflow/current",123)
 
-#hassio custom sensor
-#- platform: mqtt
-#  name: Water Flow
-#  state_topic: "greenhouse/waterflow/current"
+def send(topic, obj):
+    client.publish(topic, json.dumps(obj))
+    pass
+
+def setup():
+    client.on_publish = on_publish
+    client.username_pw_set(secrets.username, secrets.password)
+    client.connect(secrets.broker, secrets.port)
+    pass
+
+def get_waterflow_data():
+    return {"current" : 12345, "today" : 99999 }
+
+def get_watertank_data():
+    return {"temperature" : 27, "ec" : 10, "ppm" : 100 }
+
+setup()
+
+send(MQTT_TOPIC_WATERFLOW, get_waterflow_data())
+
+send(MQTT_TOPIC_WATERTANK, get_watertank_data())
